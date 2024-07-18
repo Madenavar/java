d6ab69ebf1d62690a6cc2df8b6eb36cb23c52eca
unknown <channaveermmadi@gamil.com>
2024-07-19 02:26:16 +0530

    18/07/2024

---
+++ tasks on class&object tasks/AgarBathiRunner.java
@@ -0,0 +1,116 @@
+class AgarBathiRunner{
+
+    public static void main(String[] spritualVibe){
+	
+	AgarBathi print = new AgarBathi();
+	
+	boolean creat = print.creatAgarBathi("Ullas AgarBathi",50501,15.00,"Rose",30);
+	 System.out.println("Is Item Created successfully "+creat);
+	
+	print.displayInfo();
+	
+	AgarBathi print1 = new AgarBathi();
+	
+	creat = print1.creatAgarBathi("CYCLE AgarBathi",50502,25,"Mogra",20);
+	 System.out.println("Is Item Created successfully "+creat);
+	
+	print1.displayInfo();
+	
+	AgarBathi print02 = new AgarBathi();
+	
+	creat = print02.creatAgarBathi("Divine Bliss", 1001, 50, "Sandalwood", 20);
+	 System.out.println("Is Item Created successfully "+creat);
+	
+	print02.displayInfo();
+	
+	AgarBathi print03 = new AgarBathi();
+	
+	creat = print03.creatAgarBathi("Serenity Scent", 1002, 40, "Lavender", 15);
+	 System.out.println("Is Item Created successfully "+creat);
+	
+	print03.displayInfo();
+	
+	AgarBathi print04 = new AgarBathi();
+	
+	creat = print04.creatAgarBathi("Mystic Rose", 1003, 60, "Rose", 25);
+	 System.out.println("Is Item Created successfully "+creat);
+	
+	print04.displayInfo();
+	
+	AgarBathi print05 = new AgarBathi();
+	
+	creat = print05.creatAgarBathi("Sacred Lotus", 1004, 55, "Lotus", 30);
+	 System.out.println("Is Item Created successfully "+creat);
+	
+	print05.displayInfo();
+	
+	AgarBathi print06 = new AgarBathi();
+	
+	creat = print06.creatAgarBathi("Peaceful Patchouli", 1005, 45, "Patchouli", 18);
+	 System.out.println("Is Item Created successfully "+creat);
+	
+	print06.displayInfo();
+	
+	AgarBathi print07 = new AgarBathi();
+	
+	creat = print07.creatAgarBathi("Tranquil Jasmine", 1006, 65, "Jasmine", 22);
+	 System.out.println("Is Item Created successfully "+creat);
+	
+	print07.displayInfo();
+	
+	AgarBathi print08 = new AgarBathi();
+	
+	creat = print08.creatAgarBathi("Blissful Sandal", 1007, 75, "Sandalwood", 35);
+	 System.out.println("Is Item Created successfully "+creat);
+	
+	print08.displayInfo();
+	
+	
+	AgarBathi print09 = new AgarBathi();
+	
+	creat = print09.creatAgarBathi("Divine Aroma", 1010, 70, "Frankincense", 30);
+	 System.out.println("Is Item Created successfully "+creat);
+	
+	print09.displayInfo();
+	
+	AgarBathi print10 = new AgarBathi();
+	
+	creat = print10.creatAgarBathi("Enchanted Forest", 1011, 60, "Cedarwood", 28);
+	 System.out.println("Is Item Created successfully "+creat);
+	
+	print10.displayInfo();
+	
+	AgarBathi print12 = new AgarBathi();
+	
+	creat = print12.creatAgarBathi("Spiritual Citrus", 1012, 40, "Citrus", 15);
+	 System.out.println("Is Item Created successfully "+creat);
+	
+	print12.displayInfo();
+	
+	AgarBathi print13 = new AgarBathi();
+	
+	creat = print13.creatAgarBathi("Heavenly Herbal", 1013, 45, "Herbal Blend", 18);
+	 System.out.println("Is Item Created successfully "+creat);
+	
+	print13.displayInfo();
+	
+	AgarBathi print14 = new AgarBathi();
+	
+	creat = print14.creatAgarBathi("Eternal Eucalyptus", 1014, 50, "Eucalyptus", 20);
+	 System.out.println("Is Item Created successfully "+creat);
+	
+	print14.displayInfo();
+	
+	AgarBathi print15 = new AgarBathi();
+	
+	creat = print15.creatAgarBathi("Pure Incense", 1015, 80, "Sandalwood", 40);
+	 System.out.println("Is Item Created successfully "+creat);
+	
+	print15.displayInfo();
+	
+	}
+
+
+  
+
+}
